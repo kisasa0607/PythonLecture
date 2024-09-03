0a66ba52fc706683bcfeb156fd66958da01afe38
kisasa0607 <stossm.186-326@docomo.ne.jp>
2024-09-04 01:16:40 +0900

    update if.py for mofifying

--- basic/if.py
+++ basic/if.py
@@ -1,23 +1,23 @@
 # if文
 
-age = 20
-age_alcohol = 21
-if age >= age_alcohol:
-    print("You can drink beer!")
-else:
-    print("You are too yonng to drink beer")
+#age = 20
+#age_alcohol = 21
+#if age >= age_alcohol:
+#    print("You can drink beer!")
+#else:
+#    print("You are too yonng to drink beer")
 
-age_drive = 18
+#age_drive = 18
 
-if age >= age_alcohol:
-    print("You can drink beer!")
-elif age < age_drive:
-    print("You can't even drive")
-else:
-    print("You are not allowed to drink beer but you can drive only if you have a driver's licence")
+#if age >= age_alcohol:
+#    print("You can drink beer!")
+#elif age < age_drive:
+#    print("You can't even drive")
+#else:
+#    print("You are not allowed to drink beer but you can drive only if you have a driver's licence")
 
-if not 0 < age < 120:
-    print("The value is invalid!!")
+#if not 0 < age < 120:
+#    print("The value is invalid!!")
 
 # challenge1
 
@@ -26,13 +26,13 @@ if not 0 < age < 120:
 #    balance = balance - withdraw
 #    print(balance)
 # そうでなければ、引き出せないというエラーメッセージを表示させる
-#else:
+# else:
 #    print("you can't withdraw because withdraw is bigger than balance'")
 
 # challenge2
 balance = 2000  # 残高
 withdraw = 500  # 引き出し額
-withdraw_limit = 10000  # 引き出し上限額
+withdraw_limit = 10000000  # 引き出し上限額
 # もしwithdrawがwithdraw_limitより大きい場合、引き出し上限エラーを出す
 if withdraw > withdraw_limit:
     print("You can't withdraw because withdraw is bigger than withdraw_limit")
